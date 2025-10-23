ab479911346b22a35bc400701c9fb6f7ebd53f53
Nafis Ahmed <n.ahmad.web.cit22@gmail.com>
2025-10-23 16:04:36 +0600

    test: add tests for both backward compatibility (old behavior) and secure mode of the previousPath method

--- tests/Routing/RoutingUrlGeneratorPreviousPathTest.php
+++ tests/Routing/RoutingUrlGeneratorPreviousPathTest.php
@@ -47,56 +47,184 @@ class RoutingUrlGeneratorPreviousPathTest extends TestCase
     {
         $url = $this->getUrlGenerator('http://www.foo.com/docs/api#authentication');
 
-        $this->assertSame('/docs/api', $url->previousPath());
+        $this->assertSame('/docs/api#authentication', $url->previousPath());
     }
 
-    public function testPreviousPathBlocksExternalDomain()
+    // backward compatibility tests - non-secure mode (old behavior)
+    public function testPreviousPathBackwardCompatibilityWithCrossDomain()
     {
-        $url = $this->getUrlGenerator('https://evil-site.com/malicious/path');
+        $url = $this->getUrlGenerator('http://evil.com/malicious');
 
-        $this->assertSame('/', $url->previousPath());
+        $this->assertSame('http://evil.com/malicious', $url->previousPath());
     }
 
-    public function testPreviousPathBlocksExternalDomainWithAnyPaths()
+    public function testPreviousPathBackwardCompatibilityWithJavascriptSchemes()
     {
-        $url = $this->getUrlGenerator('https://attacker.com/admin/users');
+        $url = $this->getUrlGenerator('javascript:alert("xss")');
 
-        $this->assertSame('/', $url->previousPath());
+        $this->assertSame('/javascript:alert("xss")', $url->previousPath());
     }
 
-    public function testPreviousPathBlocksDifferentScheme()
+    public function testPreviousPathBackwardCompatibilityWithDataSchemes()
+    {
+        $url = $this->getUrlGenerator('data:text/html,<script>alert("xss")</script>');
+
+        $this->assertSame('/data:text/html,<script>alert("xss")</script>', $url->previousPath());
+    }
+
+    public function testPreviousPathBackwaCompatibilityWithDifferentSchemes()
     {
         $url = $this->getUrlGenerator('https://www.foo.com/secure/area', 'www.foo.com', 'http');
 
-        $this->assertSame('/', $url->previousPath());
+        $this->assertSame('https://www.foo.com/secure/area', $url->previousPath());
     }
 
-    public function testPreviousPathAllowsSameSchemeWithPaths()
-    {
-        $url = $this->getUrlGenerator('https://www.foo.com/secures/area', 'www.foo.com', 'https');
-
-        $this->assertSame('/secures/area', $url->previousPath());
-    }
-
-    public function testPreviousPathBlocksSubdomainAttack()
+    public function testPreviousPathBackwardCompatibilityWithSubdomains()
     {
         $url = $this->getUrlGenerator('http://sub.foo.com/malicious');
 
-        $this->assertSame('/', $url->previousPath());
+        $this->assertSame('http://sub.foo.com/malicious', $url->previousPath());
     }
 
-    public function testPreviousPathBlocksDifferentPortBasedAttack()
+    public function testPreviousPathBackwardCompatibilityWithDifferentPorts()
     {
         $url = $this->getUrlGenerator('http://www.foo.com:8080/admin', 'www.foo.com', 'http');
 
-        $this->assertSame('/', $url->previousPath());
+        $this->assertSame(':8080/admin', $url->previousPath());
     }
 
-    public function testPreviousPathAllowsSameDomainWithSamePortUrl()
+    public function testPreviousPathBackwardCompatibilityWithExternalUrlAndFallback()
+    {
+        $url = $this->getUrlGenerator('https://evil.com/malicious');
+
+        $this->assertSame('https://evil.com/malicious', $url->previousPath('/dashboard'));
+    }
+
+    // secure mode tests - enhanced behavior with secure flag
+    public function testPreviousPathSecureModeWithSameDomainUrl()
+    {
+        $url = $this->getUrlGenerator('http://www.foo.com/bar/baz?secure=true');
+
+        $this->assertSame('/bar/baz', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithSameDomainUrlAndFragment()
+    {
+        $url = $this->getUrlGenerator('http://www.foo.com/docs/api#authentication');
+
+        $this->assertSame('/docs/api', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithCrossDomain()
+    {
+        $url = $this->getUrlGenerator('http://evil.com/malicious');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithJavascriptSchemes()
+    {
+        $url = $this->getUrlGenerator('javascript:alert("xss")');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithDataSchemes()
+    {
+        $url = $this->getUrlGenerator('data:text/html,<script>alert("xss")</script>');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithFileSchemes()
+    {
+        $url = $this->getUrlGenerator('file:///etc/passwd');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeWithFallback()
+    {
+        $url = $this->getUrlGenerator('http://evil.com/malicious');
+
+        $this->assertSame('/home', $url->previousPath('/home', true));
+    }
+
+    public function testPreviousPathSecureModeBlocksExternalDomain()
+    {
+        $url = $this->getUrlGenerator('https://evil-site.com/malicious/path');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksExternalDomainWithAnyPaths()
+    {
+        $url = $this->getUrlGenerator('https://attacker.com/admin/users');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksDifferentScheme()
+    {
+        $url = $this->getUrlGenerator('https://www.foo.com/secure/area', 'www.foo.com', 'http');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeAllowsSameSchemeWithPaths()
+    {
+        $url = $this->getUrlGenerator('https://www.foo.com/secures/area', 'www.foo.com', 'https');
+
+        $this->assertSame('/secures/area', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksSubdomainAttack()
+    {
+        $url = $this->getUrlGenerator('http://sub.foo.com/malicious');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksDifferentPortBasedAttack()
+    {
+        $url = $this->getUrlGenerator('http://www.foo.com:8080/admin', 'www.foo.com', 'http');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeAllowsSameDomainWithSamePortUrl()
     {
         $url = $this->getUrlGenerator('http://www.foo.com:8080/allowed', 'www.foo.com:8080', 'http');
 
-        $this->assertSame('/allowed', $url->previousPath());
+        $this->assertSame('/allowed', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksDataUri()
+    {
+        $url = $this->getUrlGenerator('data:text/html,<script>alert("xss")</script>');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksJavascriptUri()
+    {
+        $url = $this->getUrlGenerator('javascript:alert("xss")');
+
+        $this->assertSame('/', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeAllowsJavascriptUriIfOriginIsSame()
+    {
+        $url = $this->getUrlGenerator('http://www.foo.com/javascript:alert("same-origin")', 'www.foo.com');
+
+        $this->assertSame('/javascript:alert("same-origin")', $url->previousPath(false, true));
+    }
+
+    public function testPreviousPathSecureModeBlocksFileUri()
+    {
+        $url = $this->getUrlGenerator('file:///etc/passwd');
+
+        $this->assertSame('/', $url->previousPath(false, true));
     }
 
     public function testPreviousPathHandlesEmptyReferer()
@@ -113,11 +241,18 @@ class RoutingUrlGeneratorPreviousPathTest extends TestCase
         $this->assertSame('/', $url->previousPath());
     }
 
-    public function testPreviousPathWithFallbackForExternalUrl()
+    public function testPreviousPathSecureModeWithFallbackForExternalUrl()
     {
         $url = $this->getUrlGenerator('https://evil.com/malicious');
 
-        $this->assertSame('/dashboard', $url->previousPath('/dashboard'));
+        $this->assertSame('/dashboard', $url->previousPath('/dashboard', true));
+    }
+
+    public function testPreviousPathSecureModeAllowsFileUriIfOriginIsSame()
+    {
+        $url = $this->getUrlGenerator('http://www.foo.com/file:///etc/same');
+
+        $this->assertSame('/file:///etc/same', $url->previousPath(false, true));
     }
 
     public function testPreviousPathNormalizesTrailingSlashes()
@@ -141,53 +276,18 @@ class RoutingUrlGeneratorPreviousPathTest extends TestCase
         $this->assertSame('/path-with-dashes/under_scores/123', $url->previousPath());
     }
 
-    public function testPreviousPathBlocksDataUri()
-    {
-        $url = $this->getUrlGenerator('data:text/html,<script>alert("xss")</script>');
-
-        $this->assertSame('/', $url->previousPath());
-    }
-
-    public function testPreviousPathBlocksJavascriptUri()
-    {
-        $url = $this->getUrlGenerator('javascript:alert("xss")');
-
-        $this->assertSame('/', $url->previousPath());
-    }
-
-    public function testPreviousPathAllowsJavascriptUriIfOriginIsSame()
-    {
-        $url = $this->getUrlGenerator('http://www.foo.com/javascript:alert("same-origin")', 'www.foo.com');
-
-        $this->assertSame('/javascript:alert("same-origin")', $url->previousPath());
-    }
-
-    public function testPreviousPathBlocksFileUri()
-    {
-        $url = $this->getUrlGenerator('file:///etc/passwd');
-
-        $this->assertSame('/', $url->previousPath());
-    }
-
-    public function testPreviousPathAllowsFileUriIfOriginIsSame()
-    {
-        $url = $this->getUrlGenerator('http://www.foo.com/file:///etc/same');
-
-        $this->assertSame('/file:///etc/same', $url->previousPath());
-    }
-
-    public function testPreviousPathHandlesCaseInsensitiveHost()
+    public function testPreviousPathSecureModeHandlesCaseInsensitiveHost()
     {
         $url = $this->getUrlGenerator('http://WWW.FOO.COM/path', 'www.foo.com');
 
-        $this->assertSame('/path', $url->previousPath());
+        $this->assertSame('/path', $url->previousPath(false, true));
     }
 
-    public function testPreviousPathHandlesCaseVariations()
+    public function testPreviousPathSecureModeHandlesCaseVariations()
     {
         $url = $this->getUrlGenerator('http://www.FOO.com/path', 'www.foo.com');
 
-        $this->assertSame('/path', $url->previousPath());
+        $this->assertSame('/path', $url->previousPath(false, true));
     }
 
     public function testPreviousPathHandlesUrlWithoutPath()
@@ -219,19 +319,22 @@ class RoutingUrlGeneratorPreviousPathTest extends TestCase
         $method = $reflection->getMethod('isSameOrigin');
         $method->setAccessible(true);
 
-        $this->assertTrue($method->invoke($url,
+        $this->assertTrue($method->invoke(
+            $url,
             ['host' => 'www.foo.com', 'scheme' => 'http'],
             ['host' => 'www.foo.com', 'scheme' => 'http']
         ));
 
         // different host
-        $this->assertFalse($method->invoke($url,
+        $this->assertFalse($method->invoke(
+            $url,
             ['host' => 'evil.com', 'scheme' => 'http'],
             ['host' => 'www.foo.com', 'scheme' => 'http']
         ));
 
         // different scheme
-        $this->assertFalse($method->invoke($url,
+        $this->assertFalse($method->invoke(
+            $url,
             ['host' => 'www.foo.com', 'scheme' => 'https'],
             ['host' => 'www.foo.com', 'scheme' => 'http']
         ));
@@ -241,7 +344,8 @@ class RoutingUrlGeneratorPreviousPathTest extends TestCase
         $this->assertFalse($method->invoke($url, ['host' => 'www.foo.com'], ['host' => 'www.foo.com', 'scheme' => 'http']));
 
         // case insensitive
-        $this->assertTrue($method->invoke($url,
+        $this->assertTrue($method->invoke(
+            $url,
             ['host' => 'WWW.FOO.COM', 'scheme' => 'http'],
             ['host' => 'www.foo.com', 'scheme' => 'http']
         ));
