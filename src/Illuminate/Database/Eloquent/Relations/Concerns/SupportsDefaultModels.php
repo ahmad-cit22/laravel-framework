723a2dae8698b443d7996c2803e639ac3ae0ccec
Nafis Ahmed <n.ahmad.web.cit22@gmail.com>
2025-10-23 13:55:26 +0600

    feat: add foreign key validation logic when creating default model instances

--- src/Illuminate/Database/Eloquent/Relations/Concerns/SupportsDefaultModels.php
+++ src/Illuminate/Database/Eloquent/Relations/Concerns/SupportsDefaultModels.php
@@ -50,8 +50,8 @@ trait SupportsDefaultModels
 
         $instance = $this->newRelatedInstanceFor($parent);
 
-        // Mark the instance as a default.
-        $instance->markAsDefaultInstance();
+        // Set required foreign keys based on relationship type (if this instance uses ValidatesDefaultInstances trait)
+        $this->setRequiredForeignKeysForDefaultInstance($instance);
 
         if (is_callable($this->withDefault)) {
             return call_user_func($this->withDefault, $instance, $parent) ?: $instance;
@@ -62,5 +62,78 @@ trait SupportsDefaultModels
         }
 
         return $instance;
+    }
+
+    /**
+     * Set the required foreign keys for the default instance based on relationship type.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $instance
+     * @return void
+     */
+    protected function setRequiredForeignKeysForDefaultInstance(Model $instance)
+    {
+        // only continue if this instance uses the ValidatesDefaultInstances trait
+        $traitsUsed = class_uses_recursive($instance);
+        if (! in_array('Illuminate\Database\Eloquent\Concerns\ValidatesDefaultInstances', $traitsUsed)) {
+            return;
+        }
+
+        $instance->markAsDefaultInstance();
+
+        $potentialKeys = [];
+
+        if ($this instanceof \Illuminate\Database\Eloquent\Relations\BelongsTo) {
+            $potentialKeys[$this->getForeignKeyName()] = 'belongsTo';
+        } elseif ($this instanceof \Illuminate\Database\Eloquent\Relations\MorphTo) {
+            $potentialKeys[$this->getForeignKeyName()] = 'morphTo';
+            $potentialKeys[$this->getMorphType()] = 'morphTo';
+        } elseif ($this instanceof \Illuminate\Database\Eloquent\Relations\HasOne ||
+            $this instanceof \Illuminate\Database\Eloquent\Relations\HasMany) {
+            $potentialKeys[$this->getForeignKeyName()] = 'hasOneOrMany';
+        } elseif ($this instanceof \Illuminate\Database\Eloquent\Relations\MorphOne ||
+            $this instanceof \Illuminate\Database\Eloquent\Relations\MorphMany) {
+            $potentialKeys[$this->getForeignKeyName()] = 'morphOneOrMany';
+            $potentialKeys[$this->getMorphType()] = 'morphOneOrMany';
+        }
+
+        // Filter out the nullable columns
+        $requiredKeys = $this->filterNonNullableKeys($instance, $potentialKeys);
+
+        $instance->setRequiredForeignKeys($requiredKeys);
+    }
+
+    /**
+     * Filter out nullable foreign keys, keeping only non-nullable (required) ones.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $instance
+     * @param  array  $potentialKeys
+     * @return array
+     */
+    protected function filterNonNullableKeys(Model $instance, array $potentialKeys)
+    {
+        $requiredKeys = [];
+        $schema = $instance->getConnection()->getSchemaBuilder();
+        $tableName = $instance->getTable();
+
+        foreach ($potentialKeys as $columnName => $relationshipType) {
+            try {
+                $columnInfo = $schema->getColumnListing($tableName);
+
+                if (in_array($columnName, $columnInfo)) {
+                    $columns = $schema->getColumns($tableName);
+                    $columnDetails = collect($columns)->firstWhere('name', $columnName);
+
+                    if ($columnDetails && ! $columnDetails['nullable']) {
+                        $requiredKeys[$columnName] = $relationshipType;
+                    }
+                }
+            } catch (\Exception $e) {
+                // If can't determine nullability, err on the side of caution
+                // and don't require the key (avoiding false positives)
+                continue;
+            }
+        }
+
+        return $requiredKeys;
     }
 }
